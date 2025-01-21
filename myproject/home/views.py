bb536be55a6309e87ecb072afe77b22499f11138
NS9121 <50320608+NS9121@users.noreply.github.com>
2025-01-21 11:09:34 +0530

    First_version_with_the_UI_with_no_tabs
    UI_version with No Tabs for the chatbot

---
+++ myproject/home/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+# Create your views here.
+
+def index(request):
+    print(f"I am in Index function")
+    return render (request, 'index.html')
+
+
+
+def about(request):
+    return HttpResponse("This is about page")
+
+
+def services(request):
+    return HttpResponse("This is services page")
+
+
+def contact(request):
+    return HttpResponse("This is a contact page")
+
+@csrf_exempt
+def get_response(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            user_message = data.get('message','')
+
+            response_data = {"response" : f"You said {user_message}",
+                            "problem_statement" : "Network Connectivity issues",
+                            "root_cause_categorization":["Error","warning","Ignored"],
+                            "resolution_summary" :"issue resolved with the reset of the device"}
+
+            return JsonResponse(response_data)
+        except json.JSONDecodeError:
+            return JsonResponse({"error":"Invalid JSON Format"}, status=400)
+    return JsonResponse({"error":"Only POST requests are allowed"}, status = 405)
